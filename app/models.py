18d26f23a7949ec3cd0a0a72d69debe570f2d133
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-11-02 22:51:52 +0300

    models defined

--- app/models.py
+++ app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+# cloudinary
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
+
+# service model
+class Service(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+# barber model
+class Barber(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    phone = models.CharField(max_length=200, blank=True, null=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
+    services = models.ManyToManyField(Service)
+
+    def __str__(self):
+        return self.name
+
+
+
+# appointment model
+class Appointment(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    phone = models.CharField(max_length=200)
+    barber = models.ForeignKey(Barber, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    booking_date = models.DateField(default=dt.date.today)
+    booking_time = models.TimeField(default=dt.datetime.now)
+    # reschedule_date = models.DateField(default=dt.date.today)
+    # reschedule_time = models.TimeField(default=dt.datetime.now)
+    status = models.CharField(max_length=200, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
